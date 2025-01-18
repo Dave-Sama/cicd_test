57b9c734183756b65ba4c84263c06aa82770a656
Dave-Sama <davech1228@gmail.com>
2025-01-18 12:28:17 +0200

    added flake8 for linter

--- tests/test_my_code.py
+++ tests/test_my_code.py
@@ -8,10 +8,14 @@ class TestMyCode(unittest.TestCase):
         """
         Test the add_numbers function with sample inputs.
         """
+        
+        # Succeeded test cases
         self.assertEqual(add_numbers(2, 3), 5)
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
-        self.assertEqual(add_numbers(0, 0), 1)
+        
+        # Failed test case
+        #self.assertEqual(add_numbers(0, 0), 1)
 
 if __name__ == "__main__":
     unittest.main()
